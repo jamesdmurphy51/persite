22c59f29cc5e0dc55c188586736137045854881a
James Murphy <jamesdmurphy51@gmail.com>
2018-01-07 15:35:27 -0800

    fix server bug

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require('express');
 
-console.log('1');
-
 //return express app
 const app = express();
-
-console.log('2');
 
 //handle root page request
 app.get(['/', 'index.html'], function(req, res){
     res.sendFile(__dirname + '/public/index-1.html')
 });
 
-console.log('3');
-
 //.....so we dont need to create seperate paths for all the static files
 app.use(express.static('public'));
-
-console.log('4');
 
 //activate server
 
@@ -25,7 +17,6 @@ app.listen(80, '104.236.151.230', function(){
     console.log('App listening on port 80');
 });
 
-console.log('5');
 /*
 app.listen(8080, '127.0.0.1', function(){
     console.log('App listening on port 8080');
