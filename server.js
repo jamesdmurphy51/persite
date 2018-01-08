d8afbcc9661192cd4a160aa4f39b5fac4a22a97e
James Murphy <jamesdmurphy51@gmail.com>
2018-01-07 16:41:41 -0800

    add cv

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get(['/', 'index.html'], function(req, res){
     res.sendFile(__dirname + '/public/index-1.html')
 });
 
+//handle download request
+app.get('/downloadcv', function(req, res){
+    var file = __dirname + '/public/downloads/JamesMurphyCV.pdf';
+    res.download(file); // Set disposition and send it.
+  });
+
 //.....so we dont need to create seperate paths for all the static files
 app.use(express.static('public'));
 
