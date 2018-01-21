64224c0395e115c9d72ef79b133ef5587ab5d474
James Murphy <jamesdmurphy51@gmail.com>
2018-01-21 14:09:20 -0800

    nodemailer

--- server.js
+++ server.js
@@ -36,21 +36,19 @@ app.post('/contact', function(req, res){
     res.send(JSON.stringify(req.body)); //just bounce json right back
     
     //generate email
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false, //true for 465 only
         auth: {
-               xoauth2: xoauth2.createXOAuth2Generator({
-                   user: 'jamesdmurphy51@gmail.com',
-                   clientId: '425687429415-hv4us89a20t3qaearu6hanqbtvlpsaen.apps.googleusercontent.com',
-                   clientSecret: '6smTsFUrvprK6GqYZr8843WL',
-                   refreshToken: ''
-               })
-           }
+            user: 'james@jamesdavidmurphy.com',
+            pass:  'Bertcrew51@'
+        }
        });
     
     const mailOptions = {
-        from: "James <jamesdmurphy51@gmail.com>", // sender address
-        to: "jamesdmurphy51@gmail.com", // list of receivers
+        from: '"Administrator" <james@jamesdavidmurphy.com>', // sender address
+        to: 'james@jamesdavidmurphy.com', // list of receivers
         subject: "Someone has messaged your website", // Subject line
         html: "<p>Name: " + req.body.fullName + "<br>Email: " + req.body.email + "<br>Message: " + req.body.msg + "</p>"
     };
@@ -60,9 +58,8 @@ app.post('/contact', function(req, res){
         if(err)
           console.log(err)
         else
-          console.log(info);
+          console.log(info.messageId);
      });
-
 });
 
 
