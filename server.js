53ab3ccbf5c429ad53bd7e0a312882875b829ae0
James Murphy <jamesdmurphy51@gmail.com>
2018-01-20 19:27:30 -0800

    google html file

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.static('public'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
 
 //HANDLE PAGE REQS
 //root page
@@ -34,13 +35,17 @@ app.post('/contact', function(req, res){
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-               user: 'jamesdmurphy51@gmail.com',
-               pass: 'Facile51'
+               xoauth2: xoauth2.createXOAuth2Generator({
+                   user: 'jamesdmurphy51@gmail.com',
+                   clientId: '425687429415-hv4us89a20t3qaearu6hanqbtvlpsaen.apps.googleusercontent.com',
+                   clientSecret: '6smTsFUrvprK6GqYZr8843WL',
+                   refreshToken: ''
+               })
            }
        });
     
     const mailOptions = {
-        //from: 'a@gmail.com', // sender address
+        from: "James <jamesdmurphy51@gmail.com>", // sender address
         to: "jamesdmurphy51@gmail.com", // list of receivers
         subject: "Someone has messaged your website", // Subject line
         html: "<p>Name: " + req.body.fullName + "<br>Email: " + req.body.email + "<br>Message: " + req.body.msg + "</p>"
@@ -59,13 +64,12 @@ app.post('/contact', function(req, res){
 
 
 //activate server
-
+/*
 app.listen(80, '104.236.151.230', function(){
     console.log('App listening on port 80');
 });
+*/
 
-/*
 app.listen(8080, '127.0.0.1', function(){
     console.log('App listening on port 8080');
 });
-*/
